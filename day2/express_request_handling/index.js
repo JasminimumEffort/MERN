a403f52a6d1e13190eaa030421e9da6b4a1d6796
jasminimumeffort <jsnbsbigsave@gmail.com>
2022-10-06 16:10:56 +0100

    Express and middleware

--- day2/express_request_handling/index.js
+++ day2/express_request_handling/index.js
@@ -2,7 +2,29 @@ const express = require(`express`);
 
 const app = express();
 
-app.get(`/`, (req, res) => res.send(`Hello my name is!`));
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+
+
+//my wee middleware function
+const dateFun = (req, res, next) => {
+    console.log(new Date());
+    next();
+}
+
+// //request
+// app.use((req,res, next) => {
+//     console.log("Request received at", new Date());
+//     return next();
+// })
+
+// app.use((req, res, next) => {
+//     console.log("This is literally just an example");
+//     return next();
+// })
+
+app.get(`/`, (req, res) => res.send(`Hello my name is hi!`));
 
 let names = ['James', 'Brandon', 'Steve', 'Jordan', 'Harry'];
 
@@ -11,8 +33,8 @@ app.get('/getAll', (req, res) => {
     res.send(names)
 });
 
-
-app.get("/get/:id", (req, res) => {
+//gets item by id but also includes the fun little date function so posts date to console when get function is called
+app.get("/get/:id", dateFun, (req, res) => {
     res.send(names[req.params.id]);
 });
 
@@ -24,7 +46,7 @@ app.delete("/delete/:id", (req, res) => {
 
 
 //go to postman and post with a json file that I made using the above json activation
-
+//could use the bodyparser instead -> just saw this soln in the QA page example of bob v bill.
 app.use(express.json());
 
 app.post('/create', (req,res) => {
@@ -44,7 +66,8 @@ app.put('/replace/:index', (req, res) => {
     res.status(202).send(`${place} is now taken by ${name}`)
 })
 
-
+//server
 const server = app.listen(1296, () => {
     console.log(`Server started successfully on port number ${server.address().port}`);
 });
+
