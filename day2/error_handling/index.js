f0c3f23b9192ea8e7a924d79ea68581056e348e2
jasminimumeffort <jsnbsbigsave@gmail.com>
2022-10-07 11:09:39 +0100

    error handling

--- day2/error_handling/index.js
+++ day2/error_handling/index.js
@@ -1,7 +1,34 @@
 
 const express = require('express');
 
+const bodyParser = require('body-parser');
+
 const app = express();
+
+app.use(bodyParser.json());
+
+
+let dogs = [{"name":"Poppy"},{"name": "Mabel"},{"name" : "Winnie"},{"name":"Orla"},{"name" :"Bess"},{"name" : "Morley"},{"name" : "Hugo"},{"name" : "Scooby"},{"name" : "Ripley"}];
+
+
+const walkies = (req, res, next) => {
+    console.log("GATHER ROUND, WE'RE GOING ON A WALKIES");
+    next();
+};
+
+app.get('/getAll', walkies, (req, res) => {
+    res.send(dogs)
+});
+
+app.get("/get/:id", (req, res) => {
+    res.send(dogs[req.params.id]);
+});
+
+app.post("/newDog", (req, res, next) => {
+    if (!req.body.name) return next({ status: 400, message: "Missing dog"})
+    dogs.push(req.body);
+    res.status(201).send(dogs);
+});
 
 app.get('/bigError', (req, res, next) =>{
     next(Error(`This is also an error`));
