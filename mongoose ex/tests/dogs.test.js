d89008cb56ca2f901e77a1418896075d04e1747b
jasminimumeffort <jsnbsbigsave@gmail.com>
2022-10-12 12:10:40 +0100

    testing changes

--- mongoose ex/tests/dogs.test.js
+++ mongoose ex/tests/dogs.test.js
@@ -6,10 +6,28 @@ chai.use(chaiHttp); // adds the http plugin
 
 const server = require("../index"); // imports the server so I can run it in the test and send requests to it
 
+const { dogModel } = require("../myexdb");
+
 describe("dog tests", () => {
+    
+    let goodDog;
+    
+    beforeEach(async () => {
+        try{
+            await dogModel.deleteMany({});
+            goodDog = await dogModel.create({
+            "name": "Poppy"
+            });
+            goodDog = JSON.parse(JSON.stringify(goodDog));
+            console.log();
+        } catch(err) {
+            console.error(err);
+        }
+    });
+    
     it("should create a dog", (done) => {
         const newDog = {
-            "name": "Percy"
+            "name": "Bess"
         }
         chai.request(server).post("/newDog").send(newDog).end((err, res) => {
             chai.expect(err).to.be.null;
@@ -19,13 +37,24 @@ describe("dog tests", () => {
         });
     });
     it("should replace a dog using .json", (done) => {
-        const newDog = {
+        const newDog1 = {
             "name": "Skye"
         }
-        chai.request(server).patch("/newVetRecords/63468014f0b517dbe59ad6b8").send(newDog).end((err, res) => {
+        chai.request(server).patch("/newVetRecords/" + goodDog._id).send(newDog1).end((err, res) => {
             chai.expect(err).to.be.null;
             chai.expect(res.status).to.equal(200);
-            chai.expect(res.body).to.include(newDog);
+            chai.expect(res.body).to.include(newDog1);
+            done(); //tells mocha the test has finished
+        });
+    });
+    it("should replace a dog using URL", (done) => {
+        const newDog3 = {
+            "name":"Skye"
+        }
+        chai.request(server).patch("/newCollar/" + goodDog._id + "?name=Skye").end((err, res) => {
+            chai.expect(err).to.be.null;
+            chai.expect(res.status).to.equal(200);
+            chai.expect(res.body).to.include(newDog3);
             done(); //tells mocha the test has finished
         });
     });
@@ -33,27 +62,24 @@ describe("dog tests", () => {
         chai.request(server).get("/getAll").send().end((err, res) => {
             chai.expect(err).to.be.null;
             chai.expect(res.status).to.equal(200);
-            chai.expect(res.body).to.not.be.null;
+            chai.expect(res.body).to.deep.include(goodDog);
             done(); //tells mocha the test has finished
         });
 
     });
     it("should get a dog by the ID", (done) => {
-        const newDog = {
-            "name": "Percy"
-        }
-        chai.request(server).get("/petDog/63468014f0b517dbe59ad6b8").send().end((err, res) => {
+        chai.request(server).get("/petDog/" + goodDog._id).end((err, res) => {
             chai.expect(err).to.be.null;
             chai.expect(res.status).to.equal(200);
-            chai.expect(res.body).to.include(newDog);
+            chai.expect(res.body).to.include(goodDog);
             done(); //tells mocha the test has finished
         });
     });
     it("should send a dog home from its walk", (done) => {
-        chai.request(server).delete("/petDog/63468014f0b517dbe59ad6b8").send().end((err, res) => {
+        chai.request(server).delete("/sendHome/" + goodDog._id).end((err, res) => {
             chai.expect(err).to.be.null;
             chai.expect(res.status).to.equal(200);
-            chai.expect(res.body).to.include(newDog);
+            chai.expect(res.body).to.be.not.null;
             done(); //tells mocha the test has finished
         });
     });
